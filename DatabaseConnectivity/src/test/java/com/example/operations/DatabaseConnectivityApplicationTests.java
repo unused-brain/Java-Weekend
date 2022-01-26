47b470959c081faf4e2f8daea4ba0fa0d8476d2c
Tushar Dhakad <unusedbrain@Tushars-MacBook-Pro.local>
2022-01-26 10:28:56 +0530

    Connection to H2 Database

---
+++ DatabaseConnectivity/src/test/java/com/example/operations/DatabaseConnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.operations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseConnectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
